c97aaeb3774ffb0ca247098046f62547ac6af13e
parkpljec <park.pljec@gmail.com>
2024-08-15 01:27:48 +0900

    240814

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React from 'react';
 
 
+
+function Potato({fav}) {
+  return (
+  <h1>I like {fav}</h1>
+
+  );
+}
+const foodILike = [
+  {
+    name: 'kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  },
+  {
+    name: 'samgiopsal',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  },
+  {
+    name: 'bibimbap',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  }
+];
 function App() {
   return (
-    <div>hello many thanks</div>
+    <div>
+      <h3>hello</h3>
+    </div>
   );
 }
 
