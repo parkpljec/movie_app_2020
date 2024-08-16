93a7db6755703cb60a25d5b8c7e2219bd467ddd0
parkpljec <park.pljec@gmail.com>
2024-08-16 16:00:31 +0900

    240816git

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-
-
-function Potato({fav}) {
+function Food({ name }) {
   return (
-  <h1>I like {fav}</h1>
-
+  <h1>I like { name }</h1>
   );
 }
 const foodILike = [
@@ -15,17 +12,20 @@ const foodILike = [
   },
   {
     name: 'samgiopsal',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    image: 'http://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
   },
   {
     name: 'bibimbap',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
   }
 ];
+
+
+
 function App() {
   return (
     <div>
-      <h3>hello</h3>
+      {foodILike.map(dish=>(<Food name={dish.name} />))}
     </div>
   );
 }
