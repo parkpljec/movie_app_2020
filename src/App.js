5637f74f22860ab7206010a9074adc69411afe6e
parkpljec <park.pljec@gmail.com>
2024-08-19 20:42:08 +0900

    240819 2nd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Home from './routes/Home';
 import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
 
 function App(){
   return (
-    <Home />
-  )
+    <HashRouter>
+      <Route path="/home">
+      <h1>Home</h1>
+      </Route>
+      <Route path="/home/introduction">
+      <h1>Introduction</h1>
+      </Route>
+      <Route path="/about" >
+      <h1>About</h1>
+      </Route>
+    </HashRouter>
+  );
 }
 
 export default App;
